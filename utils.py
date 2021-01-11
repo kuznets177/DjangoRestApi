48067e70c8757d7a45f120208c24f96b2b96eebe
Alexey Kuznetsov <76772531+kuznets177@users.noreply.github.com>
2021-01-11 16:16:06 +0300

    Delete utils.py

--- utils.py
+++
@@ -1,9 +0,0 @@
-
-# функция, которая будет передавать нам ip пользователя
-def get_client_ip(request):
-    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-    if x_forwarded_for:
-        ip = x_forwarded_for.split(',')[0]
-    else:
-        ip = request.META.get('REMOTE_ADDR')
-    return ip
